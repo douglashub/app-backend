3d7373083ce45f02b7de00c6373b3f1509050ec5
Micael Santana <micaelparadox@gmail.com>
2025-03-18 16:04:47 -0300

    open legs

--- config/cors.php
+++ config/cors.php
@@ -7,27 +7,15 @@ return [
         'sanctum/csrf-cookie',
     ],
 
-    'allowed_origins' => [
-        // Your frontend subdomain(s)
-        'http://web.micasan.com.br',
-        'https://web.micasan.com.br',
-
-        // Local dev
-        'http://localhost:5173',
-
-        // If you still need the old Railway subdomain
-        'http://app-frontend-production-22ea.up.railway.app',
-        'https://app-frontend-production-22ea.up.railway.app',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
